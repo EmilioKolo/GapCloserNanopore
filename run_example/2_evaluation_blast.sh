74d75a43f47959c8df76b85a841f24228952754e
EmilioKolo <emilio_193@hotmail.com>
2026-02-17 11:08:55 -0300

    Add evaluation scripts for minimap alignment and blast/mafft alignment

---
+++ run_example/2_evaluation_blast.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Make the script stop if any command fails
+set -e
+
+# Create fasta files with selected regions for gap and telomeres
+python extract_fasta_regions.py \
+    --input ./input/C_glabrata_CBS138_current_chromosomes.fasta \
+    --region ChrC_C_glabrata_CBS138:95600-100700 \
+    --region ChrC_C_glabrata_CBS138:101600-106700 \
+    --output-folder results/ \
+    --output-name gap_5k.fasta
+
+# Align the gap region to the reference assembly
+python align_fastas.py \
+    --fasta1 ./results/gap_5k.fasta \
+    --fasta2 ./results/3511_gap_unmap.fasta \
+    --out-prefix ./results/alignments/3511_gap_unmap \
+    --mafft-threads 4
+python align_fastas.py \
+    --fasta1 ./results/gap_5k.fasta \
+    --fasta2 ./results/3511_gap_no_unmap.fasta \
+    --out-prefix ./results/alignments/3511_gap_no_unmap \
+    --mafft-threads 4
+
+python align_fastas.py \
+    --fasta1 ./results/gap_5k.fasta \
+    --fasta2 ./results/3512_gap_unmap.fasta \
+    --out-prefix ./results/alignments/3512_gap_unmap \
+    --mafft-threads 4
+python align_fastas.py \
+    --fasta1 ./results/gap_5k.fasta \
+    --fasta2 ./results/3512_gap_no_unmap.fasta \
+    --out-prefix ./results/alignments/3512_gap_no_unmap \
+    --mafft-threads 4
+
+python align_fastas.py \
+    --fasta1 ./results/gap_5k.fasta \
+    --fasta2 ./results/3567_gap_unmap.fasta \
+    --out-prefix ./results/alignments/3567_gap_unmap \
+    --mafft-threads 4
+python align_fastas.py \
+    --fasta1 ./results/gap_5k.fasta \
+    --fasta2 ./results/3567_gap_no_unmap.fasta \
+    --out-prefix ./results/alignments/3567_gap_no_unmap \
+    --mafft-threads 4
+
+python align_fastas.py \
+    --fasta1 ./results/gap_5k.fasta \
+    --fasta2 ./results/3569_gap_unmap.fasta \
+    --out-prefix ./results/alignments/3569_gap_unmap \
+    --mafft-threads 4
+python align_fastas.py \
+    --fasta1 ./results/gap_5k.fasta \
+    --fasta2 ./results/3569_gap_no_unmap.fasta \
+    --out-prefix ./results/alignments/3569_gap_no_unmap \
+    --mafft-threads 4
---
+++ run_example/2_evaluation_minimap.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+# Make the script stop if any command fails
+set -e
+
+# Create bam files
+minimap2 -ax map-ont \
+    ./results/3511_gap_no_unmap.fasta \
+    ./results/runs_3511_minimap_gap_no_unmap_clean.fastq \
+    | samtools sort -o ./results/3511_gap_no_unmap_minimap.bam
+minimap2 -ax map-ont \
+    ./results/3511_gap_unmap.fasta \
+    ./results/runs_3511_minimap_gap_unmap_clean.fastq \
+    | samtools sort -o ./results/3511_gap_unmap_minimap.bam
+
+minimap2 -ax map-ont \
+    ./results/3512_gap_no_unmap.fasta \
+    ./results/runs_3512_minimap_gap_no_unmap_clean.fastq \
+    | samtools sort -o ./results/3512_gap_no_unmap_minimap.bam
+minimap2 -ax map-ont \
+    ./results/3512_gap_unmap.fasta \
+    ./results/runs_3512_minimap_gap_unmap_clean.fastq \
+    | samtools sort -o ./results/3512_gap_unmap_minimap.bam
+
+minimap2 -ax map-ont \
+    ./results/3567_gap_no_unmap.fasta \
+    ./results/runs_3567_minimap_gap_no_unmap_clean.fastq \
+    | samtools sort -o ./results/3567_gap_no_unmap_minimap.bam
+minimap2 -ax map-ont \
+    ./results/3567_gap_unmap.fasta \
+    ./results/runs_3567_minimap_gap_unmap_clean.fastq \
+    | samtools sort -o ./results/3567_gap_unmap_minimap.bam
+
+minimap2 -ax map-ont \
+    ./results/3569_gap_no_unmap.fasta \
+    ./results/runs_3569_minimap_gap_no_unmap_clean.fastq \
+    | samtools sort -o ./results/3569_gap_no_unmap_minimap.bam
+minimap2 -ax map-ont \
+    ./results/3569_gap_unmap.fasta \
+    ./results/runs_3569_minimap_gap_unmap_clean.fastq \
+    | samtools sort -o ./results/3569_gap_unmap_minimap.bam
+
+# Index bam files
+samtools index ./results/3511_gap_no_unmap_minimap.bam
+samtools index ./results/3511_gap_unmap_minimap.bam
+samtools index ./results/3512_gap_no_unmap_minimap.bam
+samtools index ./results/3512_gap_unmap_minimap.bam
+samtools index ./results/3567_gap_no_unmap_minimap.bam
+samtools index ./results/3567_gap_unmap_minimap.bam
+samtools index ./results/3569_gap_no_unmap_minimap.bam
+samtools index ./results/3569_gap_unmap_minimap.bam
+
+# Coverage plots (done using CoveragePlotter: https://github.com/EmilioKolo/CoveragePlotter )
+PATH_TO_MAIN=../CoveragePlotter/main.py
+python $PATH_TO_MAIN \
+    --bam ./results/3511_gap_no_unmap_minimap.bam \
+    --fasta ./results/3511_gap_no_unmap.fasta \
+    --out ./results/3511_gap_no_unmap_coverage.png --binsize 10
+python $PATH_TO_MAIN \
+    --bam ./results/3511_gap_unmap_minimap.bam \
+    --fasta ./results/3511_gap_unmap.fasta \
+    --out ./results/3511_gap_unmap_coverage.png --binsize 10
+python $PATH_TO_MAIN \
+    --bam ./results/3512_gap_no_unmap_minimap.bam \
+    --fasta ./results/3512_gap_no_unmap.fasta \
+    --out ./results/3512_gap_no_unmap_coverage.png --binsize 10
+python $PATH_TO_MAIN \
+    --bam ./results/3512_gap_unmap_minimap.bam \
+    --fasta ./results/3512_gap_unmap.fasta \
+    --out ./results/3512_gap_unmap_coverage.png --binsize 10
+python $PATH_TO_MAIN \
+    --bam ./results/3567_gap_no_unmap_minimap.bam \
+    --fasta ./results/3567_gap_no_unmap.fasta \
+    --out ./results/3567_gap_no_unmap_coverage.png --binsize 10
+python $PATH_TO_MAIN \
+    --bam ./results/3567_gap_unmap_minimap.bam \
+    --fasta ./results/3567_gap_unmap.fasta \
+    --out ./results/3567_gap_unmap_coverage.png --binsize 10
+python $PATH_TO_MAIN \
+    --bam ./results/3569_gap_no_unmap_minimap.bam \
+    --fasta ./results/3569_gap_no_unmap.fasta \
+    --out ./results/3569_gap_no_unmap_coverage.png --binsize 10
+python $PATH_TO_MAIN \
+    --bam ./results/3569_gap_unmap_minimap.bam \
+    --fasta ./results/3569_gap_unmap.fasta \
+    --out ./results/3569_gap_unmap_coverage.png --binsize 10
