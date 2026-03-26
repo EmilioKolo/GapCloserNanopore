1fadc9915b2a45c168c5bd81a79e8f4188855342
EmilioKolo <emilio_193@hotmail.com>
2026-03-26 15:47:58 -0300

    Update anchor size to not include Ns

--- run_example/9_extract_fasta_anchors.sh
+++ run_example/9_extract_fasta_anchors.sh
@@ -5,7 +5,7 @@ set -e
 
 python extract_fasta_regions.py \
     --input ./input/C_glabrata_CBS138_current_chromosomes.fasta \
-    --region ChrC_C_glabrata_CBS138:95662-100664 \
-    --region ChrC_C_glabrata_CBS138:101662-106664 \
+    --region ChrC_C_glabrata_CBS138:95662-100662 \
+    --region ChrC_C_glabrata_CBS138:101663-106663 \
     --output-folder input/ \
     --output-name reference_anchors.fasta
